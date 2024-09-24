fd4841bb5d2e86f5378902fad9605a9ace744b53
Kelvin Charles <kelvinc1291@hotmail.com>
2024-09-24 09:29:55 -0300

    feat: Add layout.tsx

---
+++ src/app/(ui)/layout.tsx
@@ -0,0 +1,51 @@
+import { NavItem } from "@/components/nav/nav-item"
+import { NavLogout } from "@/components/nav/nav-logout"
+import { NavMyProfile } from "@/components/nav/nav-myprofile"
+import { Logo } from "@/components/ui/logo"
+import { RecommendationArea } from "@/components/ui/recommendation-area"
+import { SearchInput } from "@/components/ui/search-input"
+import { TrendingArea } from "@/components/ui/trending-area"
+import { faUser } from "@fortawesome/free-regular-svg-icons"
+import { faHouse } from "@fortawesome/free-solid-svg-icons"
+
+interface LayoutProps {
+    children: React.ReactNode
+}
+
+export default function Layout({children}: LayoutProps) {
+    return(
+        <main className="min-h-screen flex justify-center mx-auto max-w-7xl">
+            <section className="hidden lg:flex flex-col sticky top-0 h-screen w-72 px-3 border-r-2 border-gray-900">
+                <div className="flex-1 mt-6 ">
+                    <Logo size={24}/>
+                    <nav className="mt-11">
+                        <NavItem
+                            label="Página inicial"
+                            href="/home"
+                            icon={faHouse}
+                        />
+                        <NavItem
+                            label="Meu perfil"
+                            href="/profile"
+                            icon={faUser}
+                        />
+                    </nav>
+                </div>
+
+                <div className="mb-6 flex flex-col gap-4">
+                    <NavLogout/>
+                    <NavMyProfile/>
+                </div>
+            </section>
+            <section className="flex-1 max-w-lg">
+                {children}
+            </section>
+            <aside className="hidden lg:flex flex-col gap-6 static top-0 h-fi w-96 px-8 py-6 border-l-2 border-gray-900">
+                <SearchInput hideOnSearch/>
+                
+                <TrendingArea/>
+                <RecommendationArea/>
+            </aside>
+        </main>
+    )
+}

7320879e4b74ec1c29f7e86b87b061c3b6b5f9cd
Kelvin Charles <kelvinc1291@hotmail.com>
2024-09-24 09:27:35 -0300

    feat: Add component NavItem

---
+++ src/components/nav/nav-item.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { IconDefinition } from "@fortawesome/free-regular-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+interface NavItemProps {
+    label: string
+    icon: IconDefinition
+    href: string
+    active?:  boolean
+}
+
+export const NavItem = ({label, icon, href, active}: NavItemProps) => {
+    const pathName = usePathname();
+    const isMe = pathName === href
+
+
+    return (
+        <Link 
+            href={href}
+            className={`flex items-center gap-6 py-3 
+                ${active || isMe ? 'opacity-100' : 'opacity-70'}  hover:opacity-100
+            `}
+        >
+            <FontAwesomeIcon icon={icon} className="size-6"/>
+            <div className="text-lg">
+                {label}
+            </div>
+        </Link>
+    )
+}

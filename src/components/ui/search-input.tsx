0267166d092a85ff830e6d76b5db4ac94dbaed53
Kelvin Charles <kelvinc1291@hotmail.com>
2024-09-24 09:22:41 -0300

    feat: Add component SearchInput

---
+++ src/components/ui/search-input.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { Input } from "./input"
+import { useState } from "react"
+import { usePathname, useRouter } from "next/navigation"
+
+interface SearchInputProps {
+    defaultValue?: string
+    hideOnSearch?: boolean
+}
+
+export const SearchInput = ({defaultValue, hideOnSearch}: SearchInputProps) => {
+    const router = useRouter()
+    const pathName = usePathname()
+    const [searchInput, setSearchInput] = useState(defaultValue ?? '')
+
+    const handleSearchEnter = () => {
+        if(searchInput) {
+            router.push('/search?q='+encodeURIComponent(searchInput))
+        }
+    }
+    
+    if (hideOnSearch && pathName === '/search') return null
+    
+
+    return (
+        <Input
+            placeholder="Buscar"
+            icon={faMagnifyingGlass}
+            filled
+            value={searchInput}
+            onChange={t => setSearchInput(t)}
+            onEnter={handleSearchEnter}
+        />
+    )
+}

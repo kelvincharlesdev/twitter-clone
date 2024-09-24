5c178c4e141cd1a5187f41f5de3132798622ccaa
Kelvin Charles <kelvinc1291@hotmail.com>
2024-09-24 09:21:10 -0300

    feat: Add data user and types user

---
+++ src/data/user.ts
@@ -0,0 +1,13 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+    name: 'Kelvin',
+    slug: 'K10',
+    avatar:'https://ui-avatars.com/api/?background=random&name=Kelvin&size=100',
+    cover: 'https://img.freepik.com/fotos-gratis/conceito-de-viagem-completo-com-pontos-de-referencia_23-2149153258.jpg',
+    bio: 'Desenvolvedor Front End júnior a menos de um ano',
+    link: 'https://portifolio-kelvin.vercel.app/',
+    postCount: 127
+
+
+}
---
+++ src/types/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    name: string;
+    avatar:string;
+    slug: string;
+    cover?: string;
+    bio?:string;
+    link?:string
+    postCount: number
+
+}

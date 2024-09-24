a60c62a5a6ba5e5ca0f02454c3b4515aa3ce4dcc
Kelvin Charles <kelvinc1291@hotmail.com>
2024-09-24 09:24:46 -0300

    feat: Add component Button

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,20 @@
+interface ButtonProps {
+    label: string
+    onClick?: () => void
+    size: 1 | 2 | 3
+}
+
+export const Button  = ({label, onClick, size}: ButtonProps) => {
+    return(
+        <div 
+            onClick={onClick}
+            className={`flex justify-center items-center cursor-pointer bg-white text-black font-bold rounded-3xl 
+                ${size === 1 && 'h-14 text-lg'}
+                ${size === 2 && 'h-10 text-md'}
+                ${size === 3 && 'h-7 text-xs'}
+            `}
+        >
+            {label}
+        </div>
+    )
+}
